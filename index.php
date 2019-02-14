4eba919eca271171e20198ffbbb0908b31753017
Kolesnikov Dmitry - Slash <dmi56756877@mail.ru>
2019-02-14 17:18:29 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<title>Лайки и дизлайки на сайте</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+	<div class="row">
+		<div class="col-6">
+			<div class="card">
+				<input type="hidden" id="id_user" value="5">
+				<div class="card-body">
+					<h5 class="card-title">Title</h5>
+					<p class="card-text">Content</p>
+					<span class="btn btn-primary" id="like"><i class="far fa-thumbs-up"> like</i></span>
+					<span class="btn btn-primary" id="dislike"><i class="far fa-thumbs-down"> dislike</i></span>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
